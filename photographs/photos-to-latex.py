a96ce458a31c84c1d9bed0f08233de3edde9146e
Michael Gogins <michael.gogins@gmail.com>
2020-03-05 11:21:54 +0100

    Renaming and tweaking (#36).

--- photographs/photos-to-latex.py
+++ photographs/photos-to-latex.py
@@ -14,6 +14,7 @@ from exif import Image
 import io
 import os.path
 import string
+import subprocess
 import sys
 import traceback
 
@@ -21,38 +22,38 @@ image_root = "/home/mkg/Dropbox/images/"
 
 output_filename = "PhotographsContent1.tex"
 
-manifest = """c-2013-03-12_12-07-24.pdf|This is a scan of the first picture I took that I actually liked, a 35 mm slide. It was in 1968 in the back yard of my father's girlfriend Doreen's house in Taylorsville, Utah, just after sunset. I believe this was Christmas Day.
-Mick, Wendy, Jane, Michael XMas.jpg|Scan of a slide of my grandfather Milton (Mick) Swensen, my sister Wendy, my grandmother Jane, and myself, Murray, Utah, Christmas or New Years 1968 I think.
-renamed/c 2013-03-11_04-41-39.1.jpg|Scan of a slide of Elaine Constable, to whom I was briefly married, First Avenue stairs, Salt Lake City, Utah, 1971.
-renamed/c 2013-03-11_04-41-41.1.jpg|Scan of a slide, sidewalk leaves, South Temple Street, Salt Lake City, Utah, 1972.
-renamed/c 2013-03-11_04-41-44.1.jpg|Scan of a slide, Capitol Hill, Salt Lake City, Utah, 1972.
-renamed/c 2013-03-11_04-41-51.1.jpg|Scan of a slide, dry cleaners around the corner from where I lived after separating from Elaine, Salt Lake City, Utah, 1972.
-renamed/c 2013-03-11_04-41-49.1.jpg|Scan of a slide, coin laundromat, Third Avenue, Salt Lake City, Utah, 1972 or so.
-renamed/c 2013-03-11_04-26-23.1.jpg|Scan of a slide taken by a friend with my camera of myself on flute, free jazz jam in Loring Park, Minneapolis, Minnesota, 1973.
-renamed/c 2013-03-11_04-41-56.1.jpg|Scan of a slide of my sister Wendy, outside our building in the Loring Park neighborhood, Minneapolis, Minnesota, 1973.
-renamed/c 2013-03-11_04-41-52.1.jpg|Scan of a slide, Bike on porch, Lake Harriet neighborhood, Minneapolis, Minnesota, 1973.
-renamed/c 2013-03-11_04-41-57.1.jpg|Scan of a slide, my girlfriend Penny Suess, downtown Minneapolis, Minnesota, 1973.
-renamed/c 2013-03-11_04-41-36.1.jpg|Scan of a slide taken at the Minnesota State Fair, 1973.
-renamed/c 2013-03-11_04-41-58.1.jpg|Scan of a slide of my mother's sister Donna after Mick's funeral, on the street where she grew up, St. Paul, Minnesota, 1975.
-renamed/c 2013-03-11_04-26-24.2.jpg|Scan of a slide, eastern side of Mount Timpanogos, Wasatch Mountains, Utah, 1977.
-renamed/c 2013-03-11_04-41-50.1.jpg|Scan of a slide of my girlfriend Esther's friend Merrie, Salt Lake City, Utah, 1977 or so.
-Jan 76P3 22 retouched.jpg|Scan of a slide of my cousin Jared and his then wife, Salt Lake City, Utah, 1976.
-renamed/c 2013-03-12_12-22-47.jpg|Scan of a slide of Ruby Francis, Salt Lake City, Utah, I think in 1977.
-renamed/c 2013-03-11_04-41-46.1.jpg|Scan of a slide of Charlie Potts, poet, Salt Lake City, Utah, 1977 or so.
-renamed/c 2013-03-11_04-41-47.1.jpg|Scan of a slide of Memory Grove Park, Salt Lake City, Utah, winter of 1977 or so.
-c 2016-04-06_21-05-56.1.jpg|Scan of a slide of the Deep Creek Mountains, western Utah, 1977 or 1978.
+manifest = """c-2013-03-12_12-07-24.jpg|This is a scan of the first picture I took that I actually liked, a 35 mm slide. It was in 1968 in the back yard of my father's girlfriend Doreen's house in Taylorsville, Utah, just after sunset. I believe this was Christmas Day.
+Mick,_Wendy,_Jane,_Michael_XMas.jpg|Scan of a slide of my grandfather Milton (Mick) Swensen, my sister Wendy, my grandmother Jane, and myself, Murray, Utah, Christmas or New Years 1968 I think.
+renamed/c_2013-03-11_04-41-39.1.jpg|Scan of a slide of Elaine Constable, to whom I was briefly married, First Avenue stairs, Salt Lake City, Utah, 1971.
+renamed/c_2013-03-11_04-41-41.1.jpg|Scan of a slide, sidewalk leaves, South Temple Street, Salt Lake City, Utah, 1972.
+renamed/c_2013-03-11_04-41-44.1.jpg|Scan of a slide, Capitol Hill, Salt Lake City, Utah, 1972.
+renamed/c_2013-03-11_04-41-51.1.jpg|Scan of a slide, dry cleaners around the corner from where I lived after separating from Elaine, Salt Lake City, Utah, 1972.
+renamed/c_2013-03-11_04-41-49.1.jpg|Scan of a slide, coin laundromat, Third Avenue, Salt Lake City, Utah, 1972 or so.
+renamed/c_2013-03-11_04-26-23.1.jpg|Scan of a slide taken by a friend with my camera of myself on flute, free jazz jam in Loring Park, Minneapolis, Minnesota, 1973.
+renamed/c_2013-03-11_04-41-56.1.jpg|Scan of a slide of my sister Wendy, outside our building in the Loring Park neighborhood, Minneapolis, Minnesota, 1973.
+renamed/c_2013-03-11_04-41-52.1.jpg|Scan of a slide, Bike on porch, Lake Harriet neighborhood, Minneapolis, Minnesota, 1973.
+renamed/c_2013-03-11_04-41-57.1.jpg|Scan of a slide, my girlfriend Penny Suess, downtown Minneapolis, Minnesota, 1973.
+renamed/c_2013-03-11_04-41-36.1.jpg|Scan of a slide taken at the Minnesota State Fair, 1973.
+renamed/c_2013-03-11_04-41-58.1.jpg|Scan of a slide of my mother's sister Donna after Mick's funeral, on the street where she grew up, St. Paul, Minnesota, 1975.
+renamed/c_2013-03-11_04-26-24.2.jpg|Scan of a slide, eastern side of Mount Timpanogos, Wasatch Mountains, Utah, 1977.
+renamed/c_2013-03-11_04-41-50.1.jpg|Scan of a slide of my girlfriend Esther's friend Merrie, Salt Lake City, Utah, 1977 or so.
+Jan_76P3_22_retouched.jpg|Scan of a slide of my cousin Jared and his then wife, Salt Lake City, Utah, 1976.
+renamed/c_2013-03-12_12-22-47.jpg|Scan of a slide of Ruby Francis, Salt Lake City, Utah, I think in 1977.
+renamed/c_2013-03-11_04-41-46.1.jpg|Scan of a slide of Charlie Potts, poet, Salt Lake City, Utah, 1977 or so.
+renamed/c_2013-03-11_04-41-47.1.jpg|Scan of a slide of Memory Grove Park, Salt Lake City, Utah, winter of 1977 or so.
+c_2016-04-06_21-05-56.1.jpg|Scan of a slide of the Deep Creek Mountains, western Utah, 1977 or 1978.
 014.JPG|
-renamed/c 2013-03-11_04-26-22.1.jpg|Taken while hitchiking across the country.
+renamed/c_2013-03-11_04-26-22.1.jpg|Taken while hitchiking across the country.
 Scan7.jpg|Scan of a slide of the Roman Forum.
-2004-04-13-b 010.jpg|
-2004-12-18a 023.jpg|
-2004-12-18a 026.jpg|
-2005-02-28a 002.jpg|The only riding stable in Brooklyn.
-2005-05-17a 042.jpg|
-2005-07-09-a 100.jpg|
-2005-10-02-a 020.jpg|
-2005-10-10a 053.jpg|An old tree on our farm in Bovina, New York.
-2009-08-27-a 097.jpg|
+2004-04-13-b_010.jpg|
+2004-12-18a_023.jpg|
+2004-12-18a_026.jpg|
+2005-02-28a_002.jpg|The only riding stable in Brooklyn.
+2005-05-17a_042.jpg|
+2005-07-09-a_100.jpg|
+2005-10-02-a_020.jpg|
+2005-10-10a_053.jpg|An old tree on our farm in Bovina, New York.
+2009-08-27-a_097.jpg|
 DSC01131.JPG|
 DSC01240.JPG|
 DSC01577.JPG|The old ice cream stand in Stamford, New York. Replaced by a newer (but not quite as friendly and funky) version.
@@ -68,7 +69,7 @@ DSC02910.JPG|Christmas carnival in Carcassone, France.
 DSC09755.JPG|Merry-go-round on the boardwalk in Tel Aviv, Israel.
 20130604_134331.jpg|A remnant of Music Row in Manhattan.
 20130619_080155.jpg|Central Park, Manhattan.
-Heidi in Central Park.jpg|My wife Heidi, in Central Park, New York City, before we were married.
+Heidi_in_Central_Park.jpg|My wife Heidi, in Central Park, New York City, before we were married.
 SM-950U/20180216_201359.jpg|Recoleta, Buenos Aires, Argentina."""
 
 names_for_tags = {}
@@ -92,6 +93,12 @@ for line in lines:
     names_for_tags[tag] = name
     tags.append(tag)
     
+def bounding_box(pathname):
+    result = subprocess.run(["extractbb", "-O", pathname], encoding='utf-8', stdout=subprocess.PIPE)
+    result = result.stdout.split("\n")
+    result = result[2].split()
+    return ', bb= 0 0 {} {}'.format(str(result[3]), str(result[4]))    
+    
 stringio = io.StringIO()
     
 photos = manifest.split("\n")
@@ -100,13 +107,14 @@ for photo in photos:
     pathname = os.path.join(image_root, filename)
     pathname = r"" + pathname
     with open(pathname, 'rb') as image:
+        bb = bounding_box(pathname)
         image_with_metadata = Image(image)
         orientation = "None"
         try:
             orientation = str(image_with_metadata.get("orientation"))
         except:
             pass
-        print(orientation)
+        # print(orientation)
         width = 0
         height = 0
         try:
@@ -115,16 +123,13 @@ for photo in photos:
         except:
             pass
         stringio.write(r'''
-\clearpage
-''')
-        stringio.write(r'''
+
 \noindent ''' + caption)
         stringio.write(r'''
-
 \begin{lstlisting}
 ''')
-        #stringio.write(r"Filename: " + filename)
-        #stringio.write("\n\n")
+        stringio.write(r"Filename: " + filename)
+        stringio.write("\n\n")
         for tag in tags:
             try:
                 value = r"" + str(image_with_metadata.get(tag))
@@ -135,17 +140,34 @@ for photo in photos:
                     stringio.write("\n")
             except:
                 pass
-        stringio.write(r'''
-\end{lstlisting}
-
+        stringio.write(r'''\end{lstlisting}
+''')
+        if width > height:
+            stringio.write(r'''
+\begin{landscape}
+''')
+        else:
+            stringio.write(r'''
 \clearpage
+''')
+        stringio.write('''
 \includegraphics[''')
-    #if width < height:
-    #    stringio.write(r"angle=-90,")
-    stringio.write(r'''width=\linewidth,height=\textheight,keepaspectratio]{''')
-    stringio.write(r"" + pathname + "}")
-    stringio.write("\n")
+        stringio.write(r'''width=\linewidth,height=\textheight,keepaspectratio''')
+        #bb = ",bb=0 0 " + str(height) + " " + str(width)
+        stringio.write(bb)
+        stringio.write(r''']{''');
+        stringio.write(r"" + pathname + "}")
+        if width > height:
+            stringio.write(r'''
+\end{landscape}
 
+''')
+        else:
+            stringio.write('''
+\clearpage
+''')
+    
+    
 print(stringio.getvalue())
 
 output = open(output_filename, 'w')
