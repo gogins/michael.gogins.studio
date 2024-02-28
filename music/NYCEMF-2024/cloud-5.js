0b310179cc93a5e6b2d661a57fa1e8a549f16d80
gogins <michael.gogins@gmail.com>
2024-02-28 12:14:32 +0000

    Updating article and design for cloud-5.

---
+++ music/NYCEMF-2024/cloud-5.js
@@ -0,0 +1,454 @@
+/**
+ * Defines custom HTML elements for the cloud-5 system. Once a custom element 
+ * is defined in the body of the HTML, its DOM object can be obtained, and 
+ * then not only DOM methods but also custom methods can be called.
+ * 
+ * In general, rather than require users to subclass these custom elements, 
+ * although that is possible, users will define and set hook functions, code 
+ * text, and object properties.
+ * 
+ * To simplify both usage and maintenance, internal styles are usually not 
+ * used.
+ * 
+ * Usage:
+ * 1. Include the csound-5.js script in the Web page.
+ * 2. Lay out and style the required custom elements as with any other HTNL 
+ *    elements.
+ * 3. In a script element of the Web page:
+ *    a. Define hook functions and code text as JavaScript variables.
+ *    b. Obtain DOM objects from custom elements.
+ *    c. Assign hook functions, code text, and DOM objects to their 
+ *       relevant targets.
+ */
+
+/**
+ * Sets up the piece, and defines menu items as required. The overlays are 
+ * other custom elements.
+ */
+class Cloud5Piece extends HTMLElement {
+  constructor() {
+    super();
+    this.csound = null;
+    /**
+     * May be assigned the text of a Csound .csd patch. If so, Csound will be 
+     * instantiated, and the Csound patch will be compiled and run for every 
+     * performance. If the patch is null, then Csound will not be used.
+     */
+    this.csound_patch = null;
+    /**
+     * May be assigned a score generating function. If so, the score generator 
+     * will be called for each performance, and must generate and return a 
+     * CsoundAC Score, which will be translated to a Csound score in text 
+     * format, appended to the Csound patch, and played or rendered by Csound.
+     */
+    this.score_generator_hook = null;
+    /**
+     * May be assigned an instance of a cloud5-pianoroll overlay. If so, then 
+     * the Score button will be created for showing and hiding a piano roll 
+     * display of the generated CsoundAC Score.
+     */
+    this.piano_roll_overlay = null;
+    /**
+     * May be assigned an instance of a cloud5-shader overlay. If so, 
+     * the GLSL shader will run at all times, and will normally create the 
+     * background for all other overlays. The shader overlay may call 
+     * a hook function either to visualize the audio of the performance, 
+     * or to sample the video canvas to generate notes for performance by 
+     * Csound.
+     */
+    this.shader_overlay = null;
+    /**
+     * May be assigned an instance of the cloud5-log overlay. If so, 
+     * the Log button will be created for showing and hiding a scrolling 
+     * view of messages from Csound or other sources.
+     */
+    this.log_overlay = null;
+    /**
+     * May be assigned an instance of the cloud5-about overlay. If so,
+     * the About button wll be created for showing and hiding the text 
+     * of the overlay. The inner HTML of this element should contain 
+     * license information, authorship, credits, and program notes for the 
+     * piece.
+     */
+    this.about_overlay = null;
+  }
+  /**
+    * Called by the browser whenever this element is added to the 
+    * document. Construction and initialization of the element should take 
+    * place here.
+    */
+  connectedCallback() {
+    let shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.innerHTML = `<link rel="stylesheet" href="w3.css">
+    <div class="w3-bar" id="main_menu" style="position:fixed;background:transparent;z-index:100;">
+    <ul class="menu">
+        <li id="menu_item_play" title="Play piece on system audio output" class="w3-btn w3-hover-text-light-green">
+            Play</li>
+        <li id="menu_item_render" title="Render piece to soundfile" class="w3-btn w3-hover-text-light-green">Render
+        </li>
+        <li id="menu_item_stop" title="Stop performance" class="w3-btn w3-hover-text-light-green">Stop</li>
+        <li id="menu_item_fullscreen" class="w3-btn w3-hover-text-light-green">Fullscreen</li>
+        <li id="menu_item_strudel" class="w3-btn w3-hover-text-light-green">Strudel</li>
+        <li id="menu_item_score" title="Show/hide piano roll score" class="w3-btn w3-hover-text-light-green">Score
+        </li>
+        <li id="menu_item_console" title="Show/hide message console" class="w3-btn w3-hover-text-light-green">Log
+        </li>
+        <li id="menu_item_about" title="Show/hide information about this piece"
+            class="w3-btn w3-hover-text-light-green">About</li>
+        <li id="mini_console" class="w3-btn w3-text-green w3-hover-text-light-green"></li>
+        <li id="vu_meter_left" class="w3-btn w3-hover-text-light-green"></li>
+        <li id="vu_meter_right" class="w3-btn w3-hover-text-light-green"></li>
+        <li id="menu_item_dat_gui"
+            title="Show/hide performance controls; 'Save' copies all control parameters to system clipboard"
+            class="w3-btn w3-left-align w3-hover-text-light-green w3-right"></li>
+    </ul>
+</div>`;
+    let menu_item_about = shadowRoot.querySelector('#menu_item_about');
+    let host = this;
+    menu_item_about.onclick = function (event) {
+      console.log("menu_item_about click...");
+      if (host.about_overlay.checkVisibility() == true) {
+        host.about_overlay.style.display = 'none';
+      } else {
+        host.about_overlay.style.display = '';
+      }
+    };
+    menu_item_score.onclick = function (event) {
+      console.log("menu_item_score click...");
+      if (host.piano_roll_overlay.checkVisibility() == true) {
+        host.piano_roll_overlay.style.display = 'none';
+      } else {
+        host.piano_roll_overlay.style.display = '';
+      }
+    };
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+}
+customElements.define("cloud5-piece", Cloud5Piece);
+
+/**
+ * Displays a CsoundAC Score as a 3-dimensional piano roll.
+ */
+class Cloud5PianoRoll extends HTMLElement {
+  constructor() {
+    super();
+    this.silencio_score = new Silencio.Score();
+    this.csoundac_score = null;
+    this.canvas = null;
+  }
+  /**
+    * Called by the browser whenever this element is added to the document.
+    */
+  connectedCallback() {
+    let shadowRoot = this.attachShadow({ mode: "open" });
+    this.canvas = new HTMLCanvasElement();
+    shadowRoot.addChild(this.canvas);
+    if (this.csoundac_score !== null) {
+      this.draw(this.csoundac_score);
+    }
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+  draw(csoundac_score_) {
+    this.csoundac_score = csoundac_score_;
+    this.silencio_score.copyCsoundAcScore(this.csoundac_score);
+    this.silencio_score.draw3D(this.canvas);
+  }
+}
+customElements.define("cloud5-piano-roll", Cloud5PianoRoll);
+
+/**
+ * Contains an instance of the Strudel that can use Csound as an output,
+ * and starts and stops along wth Csound.
+ */
+class Cloud5Strudel extends HTMLElement {
+  constructor() {
+    super();
+    /**
+     * May be assigned the text of a Strudel patch. If so, the Strudel REPL 
+     * will be instantiated, the Strudel button will be 
+     * created for showing and hiding the Strudel REPL, and Strudel will be 
+     * stopped and started at the same time as Csound.
+     */
+    this.strudel_patch = null;
+  }
+  /**
+    * Called by the browser whenever this element is added to the document.
+    */
+  connectedCallback() {
+    this.class = "w3-container cloud5-overlay";
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+}
+customElements.define("cloud5-strudel", Cloud5Strudel);
+
+/**
+ * Presents visuals generated by a GLSL shader. These visuals can 
+ * show a visualization of the music, or be sampled to generate notes for 
+ * Csound to perform.
+ */
+class Cloud5Shader extends HTMLElement {
+  constructor() {
+    super();
+    /**
+     * The user may define a vertex shader in GLSL code for generating 
+     * visuals, and assign a string containing the code to this property.
+     */
+    this.fragment_shader = null;
+    /**
+     * This is GLSL code for a default vertex shader; the user may define a 
+     * different vertex shader and assign it to this property.
+     */
+    this.vertex_shader = `>#version 300 es
+    in vec2 inPos;
+    void main() {
+        gl_Position = vec4(inPos.xy, 0.0, 1.0);
+    }`;
+    /**
+     * The user may define a function that will be called at intervals to 
+     * receive a real-time FFT analysis of the audio; the function should 
+     * downsample and/or otherwise process the analysis to generate CsoundAC 
+     * Notes, which must be returned in a CsoundAC Score. The user-defined 
+     * function must be assigned to this property.
+     */
+    this.shader_sampler_hook = null;
+    /**
+     * The user may define a function that will be called at intervals to 
+     * receive pending Notes of the performance; the function should use these 
+     * to compute GLSL uniforms that will in some way control the appearance 
+     * and behavior of the shader visuals. The user-defined function must be 
+     */
+    this.audio_visualizer_hook = null;
+  }
+  /**
+    * Called by the browser whenever this element is added to the document.
+    */
+  connectedCallback() {
+    this.class = "w3-container cloud5-overlay";
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+}
+customElements.define("cloud5-shader", Cloud5Shader);
+
+/**
+ * Displays a scrolling list of runtime messages from Csound and/or other 
+ * sources.
+ */
+class Cloud5Log extends HTMLElement {
+  constructor() {
+    super();
+    this.csound5_piece = null;
+  }
+  /**
+    * Called by the browser whenever this element is added to the document.
+    */
+  connectedCallback() {
+    let shadowRoot = this.attachShadow({ mode: "open" });
+    this.csound = this.csound5_piece3.csound;
+    this.div = new HTMLDivElement();
+    this.div.id = "console_view";
+    shadowRoot.addChild(this.div);
+    this.message_callback_buffer = "";
+    this.console_editor = ace.edit("console_view");
+    //console_editor.setTheme("ace/theme/gob");
+    this.console_editor.setReadOnly(true);
+    this.console_editor.setShowPrintMargin(false);
+    this.console_editor.setDisplayIndentGuides(false);
+    this.console_editor.renderer.setOption("showGutter", false);
+    this.console_editor.renderer.setOption("showLineNumbers", true);
+
+    var csound_message_callback = async function (message) {
+      if (message === null) {
+        return;
+      }
+      let level_left = -100;
+      let level_right = -100;
+      if (non_csound(csound) == false) {
+        score_time = await csound.GetScoreTime();
+        level_left = await csound.GetControlChannel("gk_MasterOutput_output_level_left");
+        level_right = await csound.GetControlChannel("gk_MasterOutput_output_level_right");
+        let delta = score_time;
+        // calculate (and subtract) whole days
+        let days = Math.floor(delta / 86400);
+        delta -= days * 86400;
+        // calculate (and subtract) whole hours
+        let hours = Math.floor(delta / 3600) % 24;
+        delta -= hours * 3600;
+        // calculate (and subtract) whole minutes
+        let minutes = Math.floor(delta / 60) % 60;
+        delta -= minutes * 60;
+        // what's left is seconds
+        let seconds = delta % 60;  // in theory the modulus is not required
+        if (level_left > 0) {
+          $("#vu_meter_left").css("color", "red");
+        } else if (level_left > -12) {
+          $("#vu_meter_left").css("color", "orange")
+        } else {
+          $("#vu_meter_left").css("color", "lightgreen");
+        }
+        if (level_right > 0) {
+          $("#vu_meter_right").css("color", "red");
+        } else if (level_right > -12) {
+          $("#vu_meter_right").css("color", "orange")
+        } else {
+          $("#vu_meter_right").css("color", "lightgreen");
+        }
+        $("#mini_console").html(sprintf("d:%4d h:%02d m:%02d s:%06.3f", days, hours, minutes, seconds));
+        $("#vu_meter_left").html(sprintf("L%+7.1f dBA", level_right));
+        $("#vu_meter_right").html(sprintf("R%+7.1f dBA", level_right));
+      };
+      // Split in case the newline is in the middle of the message but 
+      // not at the end?
+      message_callback_buffer = message_callback_buffer + message;
+      if (message_callback_buffer.endsWith("\n")) {
+        console.log(message_callback_buffer);
+        let lines = console_editor.getSession().getLength();
+        // Prevent the console editor from hogging memory.
+        if (lines > 5000) {
+          console_editor.getSession().removeFullLines(0, 2500);
+          lines = console_editor.getSession().getLength();
+        }
+        console_editor.moveCursorTo(lines, 0);
+        console_editor.scrollToLine(lines);
+        console_editor.insert(message_callback_buffer);
+        message_callback_buffer = "";
+      };
+    };
+
+    var interval_id = null;
+
+    var trackScoreTime = function () {
+      if (non_csound(csound)) {
+        return;
+      }
+      var score_time = csound.getScoreTime();
+      interval_id = setTimeout(trackScoreTime, 200);
+      silencio_score.progress3D(score_time);
+    };
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+  log(text) {
+
+  }
+}
+customElements.define("cloud5-log", Cloud5Log);
+
+/**
+ * Contains license, authorship, credits, and program notes as the inner HTML 
+ * of this.
+ */
+class Cloud5About extends HTMLElement {
+  constructor() {
+    super();
+  }
+  /**
+    * Called by the browser whenever this element is added to the document.
+    */
+  connectedCallback() {
+  }
+  /**
+   * Called by the browser whenever this element is removed from the document.
+   */
+  disconnectedCallback() {
+  }
+  /**
+   * Returns an array of attribute names to monitor for changes. As the other 
+   * custom elements are not plain old data, they are properties but not 
+   * attributes and so are not observed.
+   */
+  static get observedAttributes() {
+    return [];
+  }
+  /**
+   * Called by the browser when one of the observed attributes has changed.
+   */
+  attributeChangedCallback(name, oldValue, newValue) {
+  }
+}
+customElements.define("cloud5-about", Cloud5About);
+
+
