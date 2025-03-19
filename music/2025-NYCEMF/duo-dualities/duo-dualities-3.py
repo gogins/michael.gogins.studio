bc6c14b78624452be4cc548949ed6ccf421845c2
gogins <michael.gogins@gmail.com>
2025-03-19 21:32:56 +0100

    Tweaking.

--- music/2025-NYCEMF/duo-dualities/duo-dualities-3.py
+++ music/2025-NYCEMF/duo-dualities/duo-dualities-3.py
@@ -17,16 +17,16 @@ model = CsoundAC.MusicModel()
 gcg = GeneralizedContextualGroup.GeneralizedContextualGroup()
 gcg.avoidParallelFifths = True 
  
-gcg.setAxiom('section1 dd dd dd T+2 V-13 section2 D*1.5 T+5 V-2 section3 D/1.5 ')
-gcg.addRule( 'section1', 'B3  R63 P(0,4,7) V+9 I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
-gcg.addRule( 'section2', 'B12 R41 P(0,3,7) V+3 6 L*2 D*2.25 I0=5 I1=5 I2=5 I3=5 I4=5 I5=5 T+2 V+35 a3 D/2.225 ') 
+gcg.setAxiom('D*0.76 section1 D/0.76 dd dd dd T+2 V-13 section2 D*2. T+5 V-2 section3 D/2. ')
+gcg.addRule( 'section1', 'B3  R63 P(0,4,7) V+9   I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
+gcg.addRule( 'section2', 'B12 R41 P(0,3,7) V+3 6   L*2 D*2.25 I0=5 I1=5 I2=5 I3=5 I4=5 I5=5 T+2 V+35 a3 D/2.225 ') 
 gcg.addRule( 'section3', 'B0  R70 P(0,4,7) V+110  O.15  I0=3 I1=2 I2=1 L/2 arp1 dd K D*1.875 L/4 a4 B24 arp2 arp2 dd dd V-2 WV WC WC WC WC WC ') 
   
 gcg.addRule('a3',   'V+11 a3k a3q a3 a3 WV ')  
-gcg.addRule('a3k',  'K arp WV WC ')  
-gcg.addRule('a3q',  'Q7 WV K D/1.245 WV arp1 D/1.25 WC a4q D*1.225 V+2 D*1.25 V+4 WC WC ') 
+gcg.addRule('a3k',  'K  arp WV WC ')  
+gcg.addRule('a3q',  'Q7     WV K D/1.245 WV arp1 D/1.25 WC a4q D*1.225 V+2 D*1.25 V+4 WC WC ') 
   
-gcg.addRule('a4',   'xV-9 L*2 a4k a4q D/1.128 a4 arp a3 D/1.125 a3k a4 D*1.125 D*1.125 L/2 WC ')
+gcg.addRule('a4',   'xV-9  L*2 a4k a4q D/1.28 a4 arp a3 D/1.25 a3k a4 D*1.25 D*1.25 L/2 WC ')
 gcg.addRule('a4k',  'K  WV ')
 gcg.addRule('a4q',  'Q3 WV K V+1 WC')
 
