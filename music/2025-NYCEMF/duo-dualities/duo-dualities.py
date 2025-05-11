9b83ddda133a732860c123ab0443deb26016af79
gogins <michael.gogins@gmail.com>
2025-05-11 21:12:03 +0200

    Tweaking Duo Dualities (#80).

--- music/2025-NYCEMF/duo-dualities/duo-dualities.py
+++ music/2025-NYCEMF/duo-dualities/duo-dualities.py
@@ -20,13 +20,14 @@ gcg.avoidParallelFifths = True
 gcg.setAxiom('section1 dd dd dd T+2 V-13 O.2 section2 T+5 V-7 section3 D/2.5 ') 
 gcg.addRule( 'section1', 'B3  R63 P(0,4,7) V+9 I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
 gcg.addRule( 'section2', 'B12 R45 P(0,3,5,10) V+36 L/12 D*2.25 I0=5 I1=2 I2=5 I3=5 I4=2 I5=5 T+2 V+27 a3x a5 D/2.225 V-7 L*12 ')
-gcg.addRule( 'section3', 'B0  R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4 V-4 B7 arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
+gcg.addRule( 'section3', 'B0  R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4x a6 V-4 B0 K  arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
   
-gcg.addRule('a5',   'D*1.875 a3 D/1.875 ')  
-gcg.addRule('a3',   'V+11 a3k a3q a3 a3 WV ')  
+gcg.addRule('a5',   'D*1.875 a3 D/1.875 ')   
+gcg.addRule('a3',   'V+11 a3k a3q a3 a3 WV ')   
 gcg.addRule('a3k',  'K arp WV WC ')    
 gcg.addRule('a3q',  'Q7 WV K D/1.245 WV arp1 D/1.25 WC a4q D*1.225 V+2 D*1.25 V+4 WC WC ') 
   
+gcg.addRule('a6',   'xV-9 L*2 a4k a4q D/1.128 a6 arp a3 D/1.125 a3k K T2   a6 D*1.125 D*1.125 L/2 WC ')
 gcg.addRule('a4',   'xV-9 L*2 a4k a4q D/1.128 a4 arp a3 D/1.125 a3k a4 D*1.125 D*1.125 L/2 WC ')
 gcg.addRule('a4k',  'K  WV ')
 gcg.addRule('a4q',  'Q3 WV K V+1 WC') 
