b7864c56c17f617614054b79bdbeff65ab47f88d
gogins <michael.gogins@gmail.com>
2025-05-14 16:06:09 +0200

    Tweaking Duo Dualities (#80).

--- music/2025-NYCEMF/duo-dualities/duo-dualities.py
+++ music/2025-NYCEMF/duo-dualities/duo-dualities.py
@@ -18,10 +18,10 @@ gcg = GeneralizedContextualGroup.GeneralizedContextualGroup()
 gcg.avoidParallelFifths = True 
  
 gcg.setAxiom('section1 dd dd dd T+2 V-13 O.2 section2 T+5 V-7 section3 D/2.5 ') 
-gcg.addRule( 'section1', 'B3  R63 P(0,4,7) V+9 I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
-gcg.addRule( 'section2', 'B12 R45 P(0,3,5,10) V+36 L/12 D*2.25 I0=5 I1=2 I2=5 I3=5 I4=2 I5=5 T+2 V+27 a3x a5 D/2.225 V-7 L*12 ')
-gcg.addRule( 'section3', 'B0  R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4x a6 V-4 B0 K  arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
-  
+gcg.addRule( 'section1', 'B15  R63 P(0,4,7) V+9 I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
+gcg.addRule( 'section2', 'B24  R45 P(0,3,5,10) V+36 L/12 D*2.25 I0=5 I1=2 I2=5 I3=5 I4=2 I5=5 T+2 V+27 a3x a5 D/2.225 V-7 L*12 ')
+gcg.addRule( 'section3', 'B12   R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4x a6 K V-24 B0 arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
+    
 gcg.addRule('a5',   'D*1.875 a3 D/1.875 ')   
 gcg.addRule('a3',   'V+11 a3k a3q a3 a3 WV ')   
 gcg.addRule('a3k',  'K arp WV WC ')    
@@ -43,7 +43,7 @@ gcg.generate()
 
 rescale = CsoundAC.Rescale()
 rescale.setRescale( CsoundAC.Event.TIME,       True, False, (1.0 / 40.0),   60     )
-rescale.setRescale( CsoundAC.Event.KEY,        True, False, 33,             48     )
+rescale.setRescale( CsoundAC.Event.KEY,        True, False, 24,             48     )
 rescale.setRescale( CsoundAC.Event.VELOCITY,    True, True,  60,             25     )
 rescale.setRescale( CsoundAC.Event.PAN,        True, True,   0.1,            0.8   )
 rescale.addChild(gcg) 
