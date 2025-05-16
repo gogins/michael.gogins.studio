203e407b6ebfa959b21e6fefa5b91121779200bd
gogins <michael.gogins@gmail.com>
2025-05-16 11:22:48 +0200

    Tweaking is circling, stop for now (#80).

--- music/2025-NYCEMF/duo-dualities/duo-dualities.py
+++ music/2025-NYCEMF/duo-dualities/duo-dualities.py
@@ -4,7 +4,7 @@ import math
 import os
 import string
 import sys
-import types
+import types 
 import time
 from reaper_python import *
 for p in sys.path:
@@ -20,8 +20,8 @@ gcg.avoidParallelFifths = True
 gcg.setAxiom('section1 dd dd dd T+2 V-13 O.2 section2 T+5 V-7 section3 D/2.5 ') 
 gcg.addRule( 'section1', 'B15  R63 P(0,4,7) V+9 I0=1 I1=2 I2=3 V+11 a3 dd a4 dd ') 
 gcg.addRule( 'section2', 'B24  R45 P(0,3,5,10) V+36 L/12 D*2.25 I0=5 I1=2 I2=5 I3=5 I4=2 I5=5 T+2 V+27 a3x a5 D/2.225 V-7 L*12 ')
-gcg.addRule( 'section3', 'B12   R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4x a6 K V-24 B0 arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
-    
+gcg.addRule( 'section3', 'B12   R70 P(0,4,7) V+109 O.15 I0=3 I1=2 I2=1 L*12 arp1 dd K D*1.875 L/4 a4x a6 K V-24 L*16 B0 arp2 arp2 B3 dd dd T+6 sV-2 D*2 WV WC WC WC WC WC ') 
+     
 gcg.addRule('a5',   'D*1.875 a3 D/1.875 ')   
 gcg.addRule('a3',   'V+11 a3k a3q a3 a3 WV ')   
 gcg.addRule('a3k',  'K arp WV WC ')    
